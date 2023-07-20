486f4ef2d79d492555cfa3071f27abcbc1652fcc
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2023-07-20 21:36:23 +0530

    added UserBooking

--- src/Pages/UserBooking/UserBooking.js
+++ src/Pages/UserBooking/UserBooking.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import FlightCard from '../../components/FlightCard/FlightCard';
 import "./UserBooking.css";
 import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
 
-const AirportApp = (props) => {
+const AirportApp = () => {
   const [sourceCity, setSourceCity] = useState('');
   const [destinationCity, setDestinationCity] = useState('');
   const [travelDate, setTravelDate] = useState('');
   const [flights, setFlights] = useState([]);
+
+  // let navigate = useNavigate();
+
+  // console.log(props);
 
   const handleSourceCityChange = (event) => {
     setSourceCity(event.target.value);
@@ -21,33 +26,67 @@ const AirportApp = (props) => {
     setTravelDate(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  // const token = localStorage.getItem('authorization');
+  // console.log("token:",token);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Create an object with the form data
-    const formData = {
-      sourceCity: sourceCity,
-      destinationCity: destinationCity,
-      travelDate: travelDate,
-    };
-
-    // Send the form data to the backend using a fetch or axios call
-    fetch('YOUR_BACKEND_URL', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle the response from the backend
-        setFlights(data.flights);
-      })
-      .catch((error) => {
-        // Handle any errors that occurred during the fetch
-        console.error('Error:', error);
+    try {
+      // Send the form data to the backend using Axios
+      const token = JSON.parse(localStorage.getItem('authorization'));
+      console.log(token);
+      const formData = {
+        flightOrigin: sourceCity,
+        flightDestination: destinationCity,
+        startDate: travelDate,
+      };
+      const response = await axios.post('https://devrev-assessment.onrender.com/api/flight/getflightsByDate', formData, {
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
       });
+      console.log(response.data.result);
+      // Handle the response from the backend
+      setFlights(response.data.result);
+
+      // console.log(flights);
+      // navigate('/users/userbooking');
+
+      
+    } catch (error) {
+      // Handle errors that occurred during the POST request
+      console.error('Error:', error);
+    }
+
+
+
+
+    // Create an object with the form data
+    // const formData = {
+    //   sourceCity: sourceCity,
+    //   destinationCity: destinationCity,
+    //   travelDate: travelDate,
+    // };
+
+    // // Send the form data to the backend using a fetch or axios call
+    // fetch('YOUR_BACKEND_URL', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(formData),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     // Handle the response from the backend
+    //     setFlights(data.flights);
+    //   })
+    //   .catch((error) => {
+    //     // Handle any errors that occurred during the fetch
+    //     console.error('Error:', error);
+    //   });
   };
 
   return (
--- src/components/FlightCard/FlightCard.js
+++ src/components/FlightCard/FlightCard.js
@@ -1,17 +1,26 @@
-
-
 import React from 'react';
-import './FlightCard.css'
+import './FlightCard.css';
 
 const FlightCard = ({ flight }) => {
   return (
     <div className="flight-card">
-      <p>Flight Number: {flight.flightNumber}</p>
-      <p>Source: {flight.source}</p>
-      <p>Destination: {flight.destination}</p>
-      <p>Departure Date: {flight.departureDate}</p>
+      <div className="flight-info">
+        <p>Flight Number: {flight._id}</p>
+        <p>Source: {flight.flightOrigin}</p>
+        <p>Destination: {flight.flightDestination}</p>
+        <p>Departure Date: {flight.flightStartDate}</p>
+        <p>Depature Time: {flight.flightStartTime}</p>
+        <p>Arrival Date: {flight.flightEndDate}</p>
+        <p>Arrival Time: {flight.flightEndTime}</p>
+        <p>No. of Seats: {flight.noofseats}</p>
+        <p>Fare: {flight.flightFare}</p>
+      </div>
+      <button className="book-button">Book</button>
     </div>
   );
 };
 
 export default FlightCard;
+
+
+
